708f34aed7a27820b7e9e5c02c2dc24bb2389c4d
Neil Prajapati <neilp0101@gmail.com>
2021-05-21 10:12:09 -0700

    increase length of caption

--- react/src/constants/formInput.js
+++ react/src/constants/formInput.js
@@ -1,4 +1,4 @@
 export const MAX_NAME_LEN = 100;
 export const MAX_DESCR_LEN = 100;
-export const MAX_CAPTION_LEN = 300;
-export const MAX_CREDIT_LEN = 50;
+export const MAX_CAPTION_LEN = 1000;
+export const MAX_CREDIT_LEN = 100;
